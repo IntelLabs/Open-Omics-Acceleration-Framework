959ed456fc0ea6e7b8c5d6348adbb738a227b119
Ashish Patel <ashish615@gmail.com>
2023-12-05 14:50:50 +0530

    Updated miniconda installation steps
    Error because of Linux kernel update.

--- pipelines/deepvariant-based-germline-variant-calling-fq2vcf/setup_env.sh
+++ pipelines/deepvariant-based-germline-variant-calling-fq2vcf/setup_env.sh
@@ -5,11 +5,12 @@ ABS_SCRIPT_PATH="$(realpath "${SCRIPT_PATH}")"
 #echo "Value of ABS_SCRIPT_PATH: ${ABS_SCRIPT_PATH}"
 ABS_DIRECTORY="$(dirname "${ABS_SCRIPT_PATH}")"
 #echo "Value of ABS_DIRECTORY: ${ABS_DIRECTORY}"
-
+mkdir -p ./miniconda3
+ln -s ${ABS_DIRECTORY}/miniconda3 ~/miniconda3
 
 echo "Downloading and setting up miniconda..."
 wget https://repo.anaconda.com/miniconda/Miniconda3-py39_23.3.1-0-Linux-x86_64.sh
-bash ./Miniconda3-py39_23.3.1-0-Linux-x86_64.sh -b -p ./miniconda3
+bash ./Miniconda3-py39_23.3.1-0-Linux-x86_64.sh -b -u -p ./miniconda3
 echo "Downloading and setting up miniconda...DONE"
 
 echo "Seeting up conda env named with given argument"
