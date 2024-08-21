da17d518e5476f8f5a52065643a99baec4fda439
Rahamathullah365 <rahamadh934@gmail.com>
2024-08-21 06:56:53 -0400

    added protgpt2 code

---
+++ applications/protgpt2/protgpt2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from transformers import pipeline
+
+protgpt2 = pipeline('text-generation', model="nferruz/ProtGPT2")
+
+sequences = protgpt2("<|endoftext|>", max_length=100, do_sample=True, top_k=950, repetition_penalty=1.2, num_return_sequences=10, eos_token_id=0)
+
+for seq in sequences:
+	print(seq):
