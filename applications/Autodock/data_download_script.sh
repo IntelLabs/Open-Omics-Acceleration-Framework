6b03bdbde6b1fb2b3a2f47aa7e802ff43bf75fc8
sri480673 <srilekhaspanda93@gmail.com>
2024-11-05 05:32:33 -0500

    updated data download script for the test cases

--- applications/Autodock/data_download_script.sh
+++ applications/Autodock/data_download_script.sh
@@ -1,21 +1,27 @@
 url="https://zenodo.org/records/4031961/files/data.zip?download=1"
 download_dir="./data_original"
 target_folder="$1"
-mkdir -p "$download_dir"
-echo "Downloading data.zip..."
-wget -O "$download_dir/data.zip" "$url"
-echo "Unzipping data.zip..."
-unzip "$download_dir/data.zip" -d "$download_dir"
-if [ -d "$download_dir/data" ]; then
-  echo "Nested 'data' folder found."
-  if [ -d "$download_dir/data/$target_folder" ]; then
-    cp -r "$download_dir/data/$target_folder" ./
-    echo "$target_folder folder successfully copied to the current directory."
-  else
-    echo "$target_folder folder not found inside nested 'data' directory."
-  fi
+if [ ! -d "$download_dir/data" ]; then
+    echo "Downloading data.zip..."
+    mkdir -p "$download_dir"
+    wget -O "$download_dir/data.zip" "$url"
+
+    echo "Unzipping data.zip..."
+    unzip "$download_dir/data.zip" -d "$download_dir"
+    rm -f "$download_dir/data.zip"
+
+    echo "Data downloaded and extracted to $download_dir/data"
 else
-  echo "'data' folder not found in the extracted files."
+    echo "Data already exists in $download_dir/data. Skipping download and extraction."
 fi
-rm -f "$download_dir/data.zip"
-echo "'$target_folder' folder is now copied to the current directory, and the complete original data is retained in '$download_dir/data'."
+if [ -d "$target_folder" ]; then
+    echo "The folder '$target_folder' already exists in the current directory. Skipping copy."
+else
+    if [ -d "$download_dir/data/$target_folder" ]; then
+        cp -r "$download_dir/data/$target_folder" ./
+        echo "$target_folder folder successfully copied to the current directory."
+    else
+        echo "$target_folder folder not found inside '$download_dir/data'."
+    fi
+fi
+echo "'$target_folder' folder is now available in the current directory."
