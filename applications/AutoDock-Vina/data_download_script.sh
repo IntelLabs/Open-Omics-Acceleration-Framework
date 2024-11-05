a26de10969095c090dba47009dd278b4d5e25d7c
sri480673 <srilekhaspanda93@gmail.com>
2024-11-05 05:31:44 -0500

    updated data download script for the test cases

--- applications/AutoDock-Vina/data_download_script.sh
+++ applications/AutoDock-Vina/data_download_script.sh
@@ -1,8 +1,6 @@
 url="https://zenodo.org/records/4031961/files/data.zip?download=1"
 download_dir="./data_original"
 target_folder="$1"
-
-# Check if the data has already been downloaded and extracted
 if [ ! -d "$download_dir/data" ]; then
     echo "Downloading data.zip..."
     mkdir -p "$download_dir"
@@ -16,12 +14,9 @@ if [ ! -d "$download_dir/data" ]; then
 else
     echo "Data already exists in $download_dir/data. Skipping download and extraction."
 fi
-
-# Check if the target folder exists in the current directory already
 if [ -d "$target_folder" ]; then
     echo "The folder '$target_folder' already exists in the current directory. Skipping copy."
 else
-    # Check if the target folder exists inside the extracted data and copy it
     if [ -d "$download_dir/data/$target_folder" ]; then
         cp -r "$download_dir/data/$target_folder" ./
         echo "$target_folder folder successfully copied to the current directory."
@@ -29,6 +24,4 @@ else
         echo "$target_folder folder not found inside '$download_dir/data'."
     fi
 fi
-
 echo "'$target_folder' folder is now available in the current directory."
-
